3d20eb1b7c364713d8ff0f8690eb7b9f12663d95
Yusuf Ogunjobi <1009097o@student.gla.ac.uk>
2018-01-19 18:45:15 +0000

    up to population script

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+#class represent a category
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    #similar to java toString method
+    def __str__(self):
+        return self.name
+
+#class represent a page
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    #similar to java toString method
+    def _str_(self):
+        return self.title
+
