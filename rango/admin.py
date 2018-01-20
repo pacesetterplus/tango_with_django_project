ac1bca26e3534c08c582dd01f0f2cff4904ca4b0
Yusuf Ogunjobi <1009097o@student.gla.ac.uk>
2018-01-20 12:35:09 +0000

    Chapter 5 Completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# Register your models here.
 
+# This class customizes the Admin Page
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+# Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
+
--- rango/models.py
+++ rango/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-#class represent a category
+# class represent a category
+# class represents table
+# variables represent field
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -13,7 +17,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-#class represent a page
+# class represent a page
+# class represents table
+# variables represent field
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
@@ -21,6 +27,6 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
 
     #similar to java toString method
-    def _str_(self):
+    def __str__(self):
         return self.title
 
